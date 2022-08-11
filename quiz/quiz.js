62bcffa736351fe69acbdce5c56e3ece8290bfc9
Haruka Eguchi <harukaeguchi19@gmail.com>
2022-08-11 10:19:04 +0900

    途中

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,3 +1,16 @@
-$(function(){
+'use strict';
 
-}):
+{
+  const correct_answers = [
+    {
+      index: 1,
+      value: '約79万人'
+    },
+    {
+
+    }
+  ]
+}
+
+
+console.log(document.getElementsById('js-question'));
