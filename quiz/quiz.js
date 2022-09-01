64ef75817ca787c8cc38cf57413476603c1349df
Haruka Eguchi <harukaeguchi19@gmail.com>
2022-09-01 12:07:19 +0900

    done

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -14,47 +14,53 @@
    * @type {QUIZ[]}
    */
 
-const ALL_QUIZ = [
-  {
-    question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
-    answers: ['約28万人', '約79万人', '約183万人'],
-    correctNumber: 1,
-    note: '経済産業省 2019年3月 － IT 人材需給に関する調査'
-  },
-  {
-    question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
-    answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
-    correctNumber: 2,
-  },
-  {
-    question: 'IoTとは何の略でしょう？',
-    answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
-    correctNumber: 0,
-  },
-  {
-    question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
-    answers: ['Society 5.0', 'CyPhy', 'SDGs'],
-    correctNumber: 0,
-    note: 'Society5.0 - 科学技術政策 - 内閣府'
-  },
-  {
-    question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
-    answers: ['Web3.0', 'NFT', 'メタバース'],
-    correctNumber: 0,
-  },
-  {
-    question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
-    answers: ['約2倍', '約5倍', '約11倍'],
-    correctNumber: 1,
-    note: 'Accenture Technology Vision 2021'
-  }
-];
+  const ALL_QUIZ = [
+    {
+      id: 1,
+      question: '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
+      answers: ['約28万人', '約79万人', '約183万人'],
+      correctNumber: 1,
+      note: '経済産業省 2019年3月 － IT 人材需給に関する調査'
+    },
+    {
+      id: 2,
+      question: '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
+      answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
+      correctNumber: 2,
+    },
+    {
+      id: 3,
+      question: 'IoTとは何の略でしょう？',
+      answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
+      correctNumber: 0,
+    },
+    {
+      id: 4,
+      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+      answers: ['Society 5.0', 'CyPhy', 'SDGs'],
+      correctNumber: 0,
+      note: 'Society5.0 - 科学技術政策 - 内閣府'
+    },
+    {
+      id: 5,
+      question: 'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+      answers: ['Web3.0', 'NFT', 'メタバース'],
+      correctNumber: 0,
+    },
+    {
+      id: 6,
+      question: '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？',
+      answers: ['約2倍', '約5倍', '約11倍'],
+      correctNumber: 1,
+      note: 'Accenture Technology Vision 2021'
+    }
+  ];
 
   /**
    * @description クイズコンテナーの取得
    * @type {HTMLElement}
    */
-const quizContainer = document.getElementById('js-quizContainer');
+  const quizContainer = document.getElementById('js-quizContainer');
 
   /**
    * @description クイズ１つ１つのHTMLを生成するための関数
@@ -62,25 +68,25 @@ const quizContainer = document.getElementById('js-quizContainer');
    * @param questionNumber { number }
    * @returns {string}
    */
-const createQuizHtml = (quizItem, questionNumber) => {
+  const createQuizHtml = (quizItem, questionNumber) => {
 
-  /**
-   * @description 回答の生成
-   * @type {string}
-   */
-  const answersHtml = quizItem.answers.map((answer, answerIndex) => `<li class="quiz-answer-item">
+    /**
+     * @description 回答の生成
+     * @type {string}
+     */
+    const answersHtml = quizItem.answers.map((answer, answerIndex) => `<li class="quiz-answer-item">
       <button class="choice js-answer" data-answer="${answerIndex}">
         ${answer}<div class="arrow"></div>
       </button>
     </li>`
-  ).join('');
+    ).join('');
 
-// 引用テキストの生成
-const noteHtml = quizItem.note ? `<cite class="reference">
+    // 引用テキストの生成
+    const noteHtml = quizItem.note ? `<cite class="reference">
 <i class="reference-icon"><img src="./img/reference-icon.png"></i><div class="reference-text">${quizItem.note}
 </cite>` : '';
 
-return `<section class="quiz1 js-quiz" data-quiz="${questionNumber}">
+    return `<section class="quiz1 js-quiz" data-quiz="${questionNumber}">
 <div class="quiz-box-question">
   <h2 class="quiz-box-question-title">
     <span class="Q">Q${questionNumber + 1}</span>
@@ -88,7 +94,7 @@ return `<section class="quiz1 js-quiz" data-quiz="${questionNumber}">
       class="quiztext">${quizItem.question}</span>
   </h2>
   <figure class="quiz-box-image">
-    <img src="./img/q${questionNumber + 1}-img.png" alt="">
+    <img src="./img/q${quizItem.id}-img.png" alt="">
   </figure>
 </div>
 <div class="quiz-box-answer">
@@ -106,13 +112,33 @@ return `<section class="quiz1 js-quiz" data-quiz="${questionNumber}">
 </div>
 ${noteHtml}
 </section>`
-}
+  }
+
+  /** @description シャッフル出題 
+   * @oaram arrays {Array}
+   * @returna {Array}
+  */
+  const shuffle = arrays => {
+    const array = arrays.slice();
+    for (let i = array.length - 1; i >= 0; i--) { //i = ランダムに選ぶ終点のインデックス
+      const randomIndex = Math.floor(Math.random() * (i + 1));  // randamIndex = 範囲内から選ぶランダム変数
+      [array[i], array[randomIndex]] = [array[randomIndex], array[i]]; //分割代入で二つの変数iとrandomIndexを入れ替える
+    }
+    return array;
+  }
+
+  /**
+  * @description quizArrayに並び替えたクイズを格納
+  * @type {Array}
+  */
+  const quizArray = shuffle(ALL_QUIZ)
 
   /**
    * @type {string}
    * @description 生成したクイズのHTMLを #js-quizContainer に挿入
    */
-  quizContainer.innerHTML = ALL_QUIZ.map((quizItem, index) => {
+  // ここら辺分からないいいいいいいい？？？
+  quizContainer.innerHTML = quizArray.map((quizItem, index) => {
     return createQuizHtml(quizItem, index)
   }).join('')
 
@@ -120,49 +146,50 @@ ${noteHtml}
    * @type {NodeListOf<Element>}
    * @description すべての問題を取得
    */
-  const allQuiz  = document.querySelectorAll('.js-quiz');
+  const allQuiz = document.querySelectorAll('.js-quiz');
 
-// ボタンの非活性化
+  // ボタンの非活性化
   const setDisabled = answers => {
-    answers.forEach(answer =>{
+    answers.forEach(answer => {
       answer.disabled = true;
     })
   }
 
-// true or false
-const setTitle = (target, isCorrect) => {
-  target.innerText = isCorrect ? '正解!' : '不正解...';
-}
-const setClassName = (target,isCorrect) => {
-  target.classList.add(isCorrect ? 'is-correct' : 'is-incorrect');
-}
+  // true or false
+  const setTitle = (target, isCorrect) => {
+    target.innerText = isCorrect ? '正解!' : '不正解...';
+  }
+  const setClassName = (target, isCorrect) => {
+    target.classList.add(isCorrect ? 'is-correct' : 'is-incorrect');
+  }
 
-// 問題中での処理
-allQuiz.forEach(quiz => {
-  const answers = quiz.querySelectorAll('.js-answer');
-  const selectedQuiz = Number(quiz.getAttribute('data-quiz'));
-  const answerBox = quiz.querySelector('.js-answerBox');
-  const answerTitle = quiz.querySelector('.js-answerTitle');
-  const answerText = quiz.querySelector('.js-answerText');
+  // 問題中での処理
+  allQuiz.forEach(quiz => {
+    const answers = quiz.querySelectorAll('.js-answer');
+    const selectedQuiz = Number(quiz.getAttribute('data-quiz'));
+    const answerBox = quiz.querySelector('.js-answerBox');
+    const answerTitle = quiz.querySelector('.js-answerTitle');
+    const answerText = quiz.querySelector('.js-answerText');
 
-  // clickしたらclassに追加
-  answers.forEach(answer => {
-    answer.addEventListener('click', () => {
-      answer.classList.add('is-selected');
-      const selectedAnswerNumber = Number(answer.getAttribute('data-answer'));
+    // clickしたらclassに追加
+    answers.forEach(answer => {
+      answer.addEventListener('click', () => {
+        answer.classList.add('is-selected');
+        const selectedAnswerNumber = Number(answer.getAttribute('data-answer'));
 
-      // ボタン非活性化
-      setDisabled(answers);
-        
+        // ボタン非活性化
+        setDisabled(answers);
+
         // 正解ならtrue, 不正解ならfalseをcheckCorrectに格納
-        const correctNumber = ALL_QUIZ[selectedQuiz].correctNumber
+        const correctNumber = quizArray[selectedQuiz].correctNumber
         const isCorrect = correctNumber === selectedAnswerNumber;
 
         // 回答欄にテキストやclass名を付与
-        answerText.innerText = ALL_QUIZ[selectedQuiz].answers[correctNumber];
+        answerText.innerText = quizArray[selectedQuiz].answers[correctNumber];
+        setTitle(answerTitle, isCorrect);
+        setClassName(answerBox, isCorrect);
 
-      setTitle(answerTitle, isCorrect);
-      setClassName(answerBox, isCorrect);
+        //  const quizArray = shuffle(ALL_QUIZ) で定義してるからここから問題をとってくる
       })
     })
   })
